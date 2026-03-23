3fa945e6b74e909ed5177e1d14f4fd8419868e7d
pranjugeed <ranigeed334@gmail.com>
2026-03-23 22:20:43 +0530

    commit

---
+++ Assigenment1.java
@@ -0,0 +1,31 @@
+package Function;
+
+/*function factorial (n:integer)
+    declare fact:integer
+set fact=1;
+for(int n to 1)
+    fact:=fact*1
+end for
+return fact*/
+
+/*public class Assigenment1 {
+
+       public static int factorial(int n) {
+            int fact = 1;
+
+            for(int i = 1; i <= n; i++) {
+                fact = fact * i;
+            }
+
+            return fact;
+        }
+
+        public static void main(String[] args) {
+            int num = 5;
+            int result = factorial(num);
+
+            System.out.println("Factorial = " + result);
+        }
+    }*/
+
+
